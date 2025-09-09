98aff9051ba96fbcefd6b3aece5662e392ca3d9a
Md Khairul Basar <110925216+K-Basar96@users.noreply.github.com>
2025-09-09 14:02:22 +0530

    update

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,15 +1,17 @@
 // src/pages/Dashboard.jsx
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 const Dashboard = () => {
-
+    const token = useSelector(state => state.token);
     return (
         <div>
             <div className='container-fluid position-absolute' style={{top:'50px'}}>
                 <h3 className="text-white fw-bold " >SED Dashboard</h3>
                 <div className="card px-3 mt-4">
                     <h4 className="card-title">Hello there</h4>
-                    <div className="card-title">Tihs is sample text.</div>
+                    <div className="card-title">Authenticated User: {token}</div>
+                    <div className="card-title">This is sample text.</div>
                 </div>
             </div>
         </div>
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -2,7 +2,7 @@ const reducer = (data = [], action) => {
   // console.log("reducer called", action);
   switch (action.type) {
     case "TOKEN":
-      return { ...data, token: action.token };
+      return { token: action.token, ...data };
     default:
       return data;
   }
